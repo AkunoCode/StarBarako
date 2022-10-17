d26258f5200f4a2c00694ce41eb4318bd56ede35
John Leo Echevaria <echevariajohnleo@gmail.com>
2022-10-17 11:23:42 +0800

    Added leaving item on table func

--- images.py
+++ images.py
@@ -3,6 +3,11 @@ import pygame
 #  BACKGROUND PROPERTIES -------------------------------------------------------------------------------
 bg = pygame.image.load('Images\Background.png')
 counter = pygame.image.load('Images\counter.png')
+chair = pygame.image.load('Images\Chair.png')
+chair = pygame.transform.scale(chair,(135,173))
+chair2 = pygame.transform.flip(chair,True,False)
+table = pygame.image.load('Images\Table.png')
+table = pygame.transform.scale(table,(175,145))
 
 # MENU PROPERTIES ---------------------------------------------------------------------------------------
 chalkboard = pygame.image.load('Images\Chalkboard.png')
@@ -13,6 +18,11 @@ spanish_m = pygame.image.load('Images\Food-Menu\Menu-Spanish.png')
 kape_m = pygame.image.load('Images\Food-Menu\Menu-Coffee.png')
 gatas_m = pygame.image.load('Images\Food-Menu\Menu-Milk.png')
 
+kape = pygame.transform.scale(kape_m,(48,44))
+gatas = pygame.transform.scale(gatas_m,(48,44))
+ensaymada = pygame.transform.scale(ensaymada_m,(48,44))
+pandesal = pygame.transform.scale(pandesal_m,(48,44))
+spanish = pygame.transform.scale(spanish_m,(48,44))
 # PLAYER ----------------------------------------------------------------------------------------------------
 player = pygame.image.load('Images\Player\Player_0.png')
 PlayerIMG_R = pygame.transform.scale(player,(180,320))
--- main.py
+++ main.py
@@ -18,6 +18,8 @@ clock = pygame.time.Clock()
 font_big = pygame.font.Font('Images/Pixel.ttf', 50)
 font_small = pygame.font.Font('Images/Pixel.ttf', 25)
 
+order_item = [kape,gatas,ensaymada,spanish,pandesal]
+
 def write_text(text, color,font, x, y):
     textobj = font.render(text, True, color)
     textrect = textobj.get_rect()
@@ -36,24 +38,28 @@ class Player(object):
         self.walk_count = 0
     
     def drawPlayer(self,screen):
-        if self.walk_count >=72:
+        if self.walk_count >= 32:
             self.walk_count = 0
         if self.left and self.walking:
-            screen.blit(walkL[self.walk_count//9], (self.playerX,self.playerY))
-            self.walk_count += 2
+            screen.blit(walkL[self.walk_count//8], (self.playerX,self.playerY))
+            self.walk_count += 1
         elif self.right and self.walking:
-            screen.blit(walkR[self.walk_count//9], (self.playerX,self.playerY))
-            self.walk_count += 2
+            screen.blit(walkR[self.walk_count//8], (self.playerX,self.playerY))
+            self.walk_count += 1
         elif self.left:
             screen.blit(PlayerIMG_L, (self.playerX,self.playerY))
         elif self.right:
             screen.blit(PlayerIMG_R, (self.playerX,self.playerY))
+
+            
 
 # GAME LOOP -----------------------------------------------------------------------------------
 def main_game():
     running = True
     main_player = Player(10,245)
     main_player.right = True
+    order = False
+    leave = False
     while running:
 
         clock.tick(56)
@@ -65,22 +71,25 @@ def main_game():
         
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    main_player.playerX_Change = - 5
+                    main_player.playerX_Change = - 4.5
                     main_player.right = False
                     main_player.left = True
                     main_player.walking = True
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                    main_player.playerX_Change = 5
+                    main_player.playerX_Change = 4.5
                     main_player.left = False
                     main_player.right = True
                     main_player.walking = True
-                if event.key == pygame.K_w:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
                     select = True
             if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    select = False
                 if event.key == pygame.K_LEFT or event.key == pygame.K_a or event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     main_player.playerX_Change = 0.0
                     main_player.walking = False
                     main_player.walk_count = 0
+        
         
         # Update the X-Axis Position of the Player
         main_player.playerX += main_player.playerX_Change
@@ -90,11 +99,16 @@ def main_game():
             main_player.playerX = 10
         elif main_player.playerX >= 1010: # 675 because the player image is 125width (800 - 125 = 675)
             main_player.playerX = 1010
-
         if main_player.playerX >= 930 and select:
-            selection_menu()
+            order_number = selection_menu()
+            order = True
 
-        main_player.walk_count
+        # LEAVING ITEM ON THE TABLE
+        if (main_player.playerX >= 10 and main_player.playerX <= 540) and select and order:
+            leave = True
+            order = False
+            left_order = order_item[order_number]
+
     
         # DRAW SECTION --------------------------------------------------------------------------------------------
         screen.blit(bg,(0,0))
@@ -104,7 +118,27 @@ def main_game():
         screen.blit(ensaymada_m,(880,150))
         screen.blit(spanish_m,(980,150))
         screen.blit(pandesal_m,(1080,150))
+        
+        screen.blit(chair,(60,390))
+        screen.blit(table,(210,420))
+        screen.blit(chair2,(400,390))
+
+        # LEAVING ITEM ON TABLE
+        if leave:
+            screen.blit(left_order,(275,380))
+
         main_player.drawPlayer(screen)
+        
+        # PICKING UP ITEM ON THE MENU
+        if order:
+            if main_player.left:
+                item_x = main_player.playerX
+                item_y = main_player.playerY + (44*4)
+            if main_player.right:
+                item_x = main_player.playerX + (120)
+                item_y = main_player.playerY + (44*4)
+            screen.blit(order_item[order_number],(item_x,item_y))
+
         screen.blit(counter,(650,375))
         write_text("Today's Menu",(255,255,255),font_small,847,100)
         pygame.display.update()
@@ -129,15 +163,15 @@ def selection_menu():
                 choosing = False
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_s:
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                     choosing = False
-                if event.key == pygame.K_w:
-                    return (chosen_index)
-                if event.key == pygame.K_d:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    return chosen_index
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                     chosen_index += 1
                     if chosen_index > 4:
                         chosen_index = 0
-                if event.key == pygame.K_a:
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                     chosen_index -= 1
                     if chosen_index < 0:
                         chosen_index = 4
